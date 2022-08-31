dfcbcba1a01c5538b71bc5a84468b8135348a03a
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-08-31 09:34:28 +0200

    add sercet variables to connect the azure cli to the azure cloud

--- terraform-src/main.tf
+++ terraform-src/main.tf
@@ -1,9 +1,36 @@
-provider "azurerm" {
-  features {}
+terraform {
+  required_version = ">= 1.1.7"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.0.0, < 2.60.0"
+    }
+  }
 }
 
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  features {}
+
+}
 
 resource "azurerm_resource_group" "state_resource_group" {
   name     = "RG-Terraform-on-Azure"
   location = "West Europe"
+}
+
+variable "subscription_id" {
+  default = ""
+}
+variable "client_id" {
+  default = ""
+}
+variable "client_secret" {
+  default = ""
+}
+variable "tenant_id" {
+  default = ""
 }
