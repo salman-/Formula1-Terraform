0c005ddccd7c747112bcf7111830753126ebf380
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-09-09 15:32:33 +0200

    remove unused variables

--- terraform-src/main.tf
+++ terraform-src/main.tf
@@ -8,16 +8,3 @@ resource "azurerm_resource_group" "state_resource_group" {
   name     = "RG-Terraform-on-Azure"
   location = "West Europe"
 }
-
-variable "subscription_id" {
-  default = ""
-}
-variable "client_id" {
-  default = ""
-}
-variable "client_secret" {
-  default = ""
-}
-variable "tenant_id" {
-  default = ""
-}
