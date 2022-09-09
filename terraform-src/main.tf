5a79e6d642eb5320f2e51723ccf1fb72ec97b2dc
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-09-09 15:28:23 +0200

    remove attributes

--- terraform-src/main.tf
+++ terraform-src/main.tf
@@ -1,8 +1,4 @@
 provider "azurerm" {
-  subscription_id = "48ee300d-8738-496a-9366-1271ebefc1e6"
-  client_id       = "dd381b2e-4e26-4a29-96c8-0b625d6ad2e9"
-  client_secret   = "S.48Q~hXPRC~gWNPDqLRE2YvIs8RY5jjQDN3na7I"
-  tenant_id       = "1f2519d6-a33d-4336-abb5-8085bcb587d6"
 
   features {}
 
