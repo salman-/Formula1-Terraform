767d1b45dbf03f70e0b0d0dd7438c705474d4ab1
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-10-09 17:49:40 +0200

    use locals in names of resources

--- terraform-src/modules/database/mssql_database.tf
+++ terraform-src/modules/database/mssql_database.tf
@@ -1,17 +1,15 @@
 
 resource "azurerm_mssql_server" "sql_server_for_price_prediction" {
-  name                         = "sqlserverforforex"
+  name                         = "${local.prefix_database_server}-forex-price"
   resource_group_name          = var.resource_group_name
   location                     = var.resource_group_location
   version                      = "12.0"
   administrator_login          = "4dm1n157r470r"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd"
-
 }
 
 resource "azurerm_mssql_database" "sql_database_for_price_prediction" {
-  name         = "sqldatabaseforforex"
+  name         = "${local.prefix_database}-forex-price"
   server_id    = azurerm_mssql_server.sql_server_for_price_prediction.id
-  max_size_gb  = 2
   sku_name     = "basic"
 }
