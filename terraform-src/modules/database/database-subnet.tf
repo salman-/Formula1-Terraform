b8a58ec7e355c947ce8d111fd5303152ff01d011
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-10-09 17:48:58 +0200

    Add network-rule to attach mssql-server to subnet

---
+++ terraform-src/modules/database/database-subnet.tf
@@ -0,0 +1,13 @@
+resource "azurerm_subnet" "database_subnet" {
+  name                 = "${local.prefix_database_server}-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.vnet_name
+  address_prefixes     = ["10.0.3.0/24"]
+  service_endpoints    = ["Microsoft.Sql"]
+}
+
+resource "azurerm_mssql_virtual_network_rule" "database_network_rule" {
+  name      = "sql-vnet-rule"
+  server_id = azurerm_mssql_server.sql_server_for_price_prediction.id
+  subnet_id = azurerm_subnet.database_subnet.id
+}
