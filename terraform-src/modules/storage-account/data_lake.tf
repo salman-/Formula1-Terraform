f315940241b48ec2a8b2914aa55a01acfc084792
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-10-30 17:51:14 +0100

    Delete unused file

--- terraform-src/modules/storage-account/data_lake.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_storage_data_lake_gen2_filesystem" "data_lake_storage" {
-  name               = var.datalake_name
-  storage_account_id = azurerm_storage_account.storage_account.id
-
-  properties = {
-    hello = "aGVsbG8="
-  }
-}
