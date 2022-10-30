092bcb3b048c177ff9477bd6d08c814bce0f2f7a
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-10-30 23:22:13 +0100

    remove unused firewalls

--- terraform-src/modules/data-bricks/databricks-firewals.tf
+++ terraform-src/modules/data-bricks/databricks-firewals.tf
@@ -8,14 +8,3 @@ resource "azurerm_subnet_network_security_group_association" "private_nsg_asso" 
   subnet_id                 = azurerm_subnet.private_snet.id
   network_security_group_id = azurerm_network_security_group.private_empty_nsg.id
 }
-
-resource "azurerm_network_security_group" "public_empty_nsg" {
-  name                = "firewall-public"
-  location            = var.resource_group_location
-  resource_group_name = var.resource_group_name
-}
-
-resource "azurerm_subnet_network_security_group_association" "public_nsg_asso" {
-  subnet_id                 = azurerm_subnet.public_snet.id
-  network_security_group_id = azurerm_network_security_group.public_empty_nsg.id
-}
