12c61d05ba193551f270baa587fad61e8be36884
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-10-30 19:49:05 +0100

    add file which contains wrl of csv data to build pipelines in datafactory

--- terraform-src/modules/storage-account/storage_account.tf
+++ terraform-src/modules/storage-account/storage_account.tf
@@ -6,10 +6,35 @@ resource "azurerm_storage_account" "storage_account" {
   account_replication_type = "LRS"
   account_kind             = "StorageV2"
   is_hns_enabled           = "true"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
-resource "azurerm_storage_container" "container" {
-  name                  = var.container_name
+resource "azurerm_storage_data_lake_gen2_filesystem" "data_lake_storage" {
+  name               = "rawdata"
+  storage_account_id = azurerm_storage_account.storage_account.id
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_storage_container" "config_file_container" {
+  name                  = "urlofdatafiles"
   storage_account_name  = azurerm_storage_account.storage_account.name
   container_access_type = "private"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_storage_blob" "url_of_csv_data" {
+  name                   = "config-file-data"
+  storage_account_name   = azurerm_storage_account.storage_account.name
+  storage_container_name = azurerm_storage_container.config_file_container.name
+  type                   = "Block"
+  source                 = "./modules/storage-account/configFiles/ecdc_file_list.json"
 }
--- terraform-src/pipeline.tf
+++ terraform-src/pipeline.tf
@@ -10,8 +10,6 @@ resource "azurerm_resource_group" "main_resource_group" {
 
 module "storage_account" {
   source = "./modules/storage-account"
-
-  vnet_name = azurerm_virtual_network.virtual_network.name
 }
 
 module "datafactory" {
@@ -26,10 +24,11 @@ module "databricks" {
   virtual_network_id      = azurerm_virtual_network.virtual_network.id
   vnet_name               = azurerm_virtual_network.virtual_network.name
 }
-
+/*
 module "database" {
   source                  = "./modules/database"
   resource_group_name     = azurerm_resource_group.main_resource_group.name
   resource_group_location = azurerm_resource_group.main_resource_group.location
   vnet_name               = azurerm_virtual_network.virtual_network.name
 }
+*/
