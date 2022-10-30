9a349f7430615d7baef518a71bac52083bc99b6f
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-10-30 23:02:04 +0100

    parameterise the range of ip in storage subnet

---
+++ terraform-src/modules/storage-account/storage_account_subnet.tf
@@ -0,0 +1,7 @@
+resource "azurerm_subnet" "storage_account_subnet" {
+  name                 = "storage_account_subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.vnet_name
+  address_prefixes     = [var.subnet_ip_prefix]
+  service_endpoints    = ["Microsoft.Storage"]
+}
--- terraform-src/modules/storage-account/variables.tf
+++ terraform-src/modules/storage-account/variables.tf
@@ -12,3 +12,8 @@ variable "vnet_name" {
   type    = string
   default = "my-vnet"
 }
+
+variable "subnet_ip_prefix" {
+  type    = string
+  default = "10.0.2.0/24"
+}
