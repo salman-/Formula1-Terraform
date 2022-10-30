881667097d426b6213fe274e83157696262bfac1
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-10-30 19:05:30 +0100

    remove unused variables

--- terraform-src/modules/storage-account/variables.tf
+++ terraform-src/modules/storage-account/variables.tf
@@ -7,27 +7,3 @@ variable "resource_group_location" {
   type    = string
   default = "West Europe"
 }
-
-variable "container_name" {
-  type    = string
-  default = "pandemicstorageaccount"
-}
-
-variable "datalake_name" {
-  type    = string
-  default = "rawpandemicdata"
-}
-
-variable "vnet_name" {
-  type    = string
-  default = "my-vnet"
-}
-
-variable "virtual_network_id" {
-  type    = string
-  default = "To be set"
-}
-
-locals {
-  prefix = "datalake"
-}
