3e7718b066a973c05864d7ebf00444d33138d093
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-10-30 23:01:11 +0100

    remove unused subnet

--- terraform-src/modules/storage-account/subnet.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_subnet" "example" {
-  name                 = "subnetname"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = var.vnet_name
-  address_prefixes     = ["10.0.2.0/24"]
-  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
-}
