dd80368c3f778d6f48ec3883b3ccd7f0e8fc6710
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-10-31 00:04:07 +0100

    update ips

--- terraform-src/modules/data-bricks/databricks-subnet.tf
+++ terraform-src/modules/data-bricks/databricks-subnet.tf
@@ -17,7 +17,7 @@ resource "azurerm_subnet" "public_snet" {
   name                 = "subnet-public"
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.vnet_name
-  address_prefixes     = [var.subnet_ip_prefix]
+  address_prefixes     = ["10.0.3.0/24"]
 
   delegation {
     name = "databrickspublicdelegation"
--- terraform-src/modules/database/database-subnet.tf
+++ terraform-src/modules/database/database-subnet.tf
@@ -2,7 +2,7 @@ resource "azurerm_subnet" "database_subnet" {
   name                 = "${local.prefix_database_server}-subnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.vnet_name
-  address_prefixes     = ["10.0.3.0/24"]
+  address_prefixes     = ["10.0.4.0/24"]
   service_endpoints    = ["Microsoft.Sql"]
 }
 
