4d7a7387083b54e1648193d174981656f8117e0c
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-11-03 09:49:36 +0100

    rename datafactory

--- terraform-src/modules/data-factory/data_factory.tf
+++ terraform-src/modules/data-factory/data_factory.tf
@@ -1,5 +1,5 @@
 resource "azurerm_data_factory" "data_factory" {
-  name                = "datafactoryforexprice"
+  name                = "pandemicdatafactory"
   resource_group_name = var.resource_group_name
   location            = var.resource_group_location
 }
