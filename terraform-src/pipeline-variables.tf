bf3765df2c6cb70594646b39d6ad1a71264dae85
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-11-08 09:15:09 +0100

    change pipeline config

--- terraform-src/pipeline-variables.tf
+++ terraform-src/pipeline-variables.tf
@@ -8,12 +8,30 @@ variable "resource_group_location" {
   default = "West Europe"
 }
 
-variable "prefix" {
-  type    = string
-  default = "demo"
+variable "virtual_network_address_space" {
+  default = ""
 }
-
 variable "vnet_name" {
   type    = string
   default = "General-Virtual-Network"
+}
+
+variable "client_id" {
+  type =string
+  default = "dd381b2e-4e26-4a29-96c8-0b625d6ad2e9"
+}
+
+variable "client_secret" {
+  type =string
+  default = "ead9c78c-61e0-44f6-9438-b8f1c5b95ae2"
+}
+
+variable "subscription_id" {
+  type =string
+  default = "48ee300d-8738-496a-9366-1271ebefc1e6"
+}
+
+variable "tenant_id" {
+  type =string
+  default = "1f2519d6-a33d-4336-abb5-8085bcb587d6"
 }
--- terraform-src/pipeline.tf
+++ terraform-src/pipeline.tf
@@ -1,6 +1,10 @@
 provider "azurerm" {
 
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 resource "azurerm_resource_group" "main_resource_group" {
@@ -8,14 +12,25 @@ resource "azurerm_resource_group" "main_resource_group" {
   location = "West Europe"
 }
 
-module "storage_account" {
-  source           = "./modules/storage-account"
-  vnet_name        = azurerm_virtual_network.virtual_network.name
-  //subnet_ip_prefix = "10.0.1.0/24"
+module "virtual_network" {
+  source                        = "./modules/virtual_network"
+  vnet_name                     = "General-Virtual-Network"
+  virtual_network_address_space = "10.0.0.0/16"
 }
 
-module "datafactory" {
-  source = "./modules/data-factory"
+module "virtual_machine" {
+  source           = "./modules/virtual-machine"
+  vnet_name        = module.virtual_network.virtual_network_name
+  network_ip_range = module.virtual_network.virtual_network_ip_range
+  subnet_ip_range  = "10.0.1.0/24"
+}
+/*
+module "storage_account" {
+  source               = "./modules/storage-account"
+  vnet_name            = module.virtual_network.virtual_network_name
+  network_ip_range     = module.virtual_network.virtual_network_ip_range
+  subnet_ip_range      = "10.0.2.0/24"
+  storage_account_name = "formuleinsstorage"
 }
 
 module "databricks" {
@@ -25,10 +40,27 @@ module "databricks" {
   resource_group_location = azurerm_resource_group.main_resource_group.location
   virtual_network_id      = azurerm_virtual_network.virtual_network.id
   vnet_name               = azurerm_virtual_network.virtual_network.name
-  //subnet_ip_prefix        = "10.0.2.0/24"
+  databricks_name         = "formula1-databricks"
+  subnet_ip_prefix        = "10.0.2.0/24"
 }
 
-/*
+module "keyvault" {
+  source = "./modules/key-vault"
+
+  vnet_name       = azurerm_virtual_network.virtual_network.name
+  keyvault_name   = "formuleeinsvault"
+  subnet_ip_range = "10.0.3.0/24"
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+}
+
+module "datafactory" {
+  source = "./modules/data-factory"
+}
+
 module "database" {
   source                  = "./modules/database"
   resource_group_name     = azurerm_resource_group.main_resource_group.name
