efb2e3607bac1b837a28d1b6b4b297cb44260f4e
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-11-13 00:16:41 +0100

    remove unsecured default values of variables

--- terraform-src/modules/key-vault/variables.tf
+++ terraform-src/modules/key-vault/variables.tf
@@ -10,17 +10,10 @@ variable "resource_group_location" {
 
 variable "vnet_name" {
   type = string
-  default = "pndemic-vnet"
-}
-
-variable "virtual_network_id" {
-  type =string
-  default = "To be set"
 }
 
 variable "subnet_ip_range" {
   type =string
-  default = "10.0.2.0/24"
 }
 
 variable "keyvault_name" {
@@ -29,20 +22,16 @@ variable "keyvault_name" {
 
 variable "client_id_value" {
   type =string
-  default = "dd381b2e-4e26-4a29-96c8-0b625d6ad2e9"
 }
 
 variable "client_secret_value" {
   type =string
-  default = "ead9c78c-61e0-44f6-9438-b8f1c5b95ae2"
 }
 
 variable "subscription_id_value" {
   type =string
-  default = "48ee300d-8738-496a-9366-1271ebefc1e6"
 }
 
 variable "tenant_id_value" {
   type =string
-  default = "1f2519d6-a33d-4336-abb5-8085bcb587d6"
 }
