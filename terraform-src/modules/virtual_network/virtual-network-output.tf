e0ab3efd6823dfa3f93ad73429b8fda7b0246a36
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-11-12 20:39:13 +0100

    return id of virtual network to use it in other modules

--- terraform-src/modules/virtual_network/virtual-network-output.tf
+++ terraform-src/modules/virtual_network/virtual-network-output.tf
@@ -5,3 +5,7 @@ output "virtual_network_name" {
 output "virtual_network_ip_range" {
   value = azurerm_virtual_network.virtual_network.address_space[0]
 }
+
+output "virtual_network_id" {
+  value = azurerm_virtual_network.virtual_network.id
+}
