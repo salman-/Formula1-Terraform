14924216461250087ae5c7bc43a307dee1a06a89
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-11-12 22:50:52 +0100

    rename variables

--- terraform-src/modules/key-vault/variables.tf
+++ terraform-src/modules/key-vault/variables.tf
@@ -27,22 +27,22 @@ variable "keyvault_name" {
   default = "formuleeinsvault"
 }
 
-variable "client_id" {
+variable "client_id_value" {
   type =string
   default = "dd381b2e-4e26-4a29-96c8-0b625d6ad2e9"
 }
 
-variable "client_secret" {
+variable "client_secret_value" {
   type =string
   default = "ead9c78c-61e0-44f6-9438-b8f1c5b95ae2"
 }
 
-variable "subscription_id" {
+variable "subscription_id_value" {
   type =string
   default = "48ee300d-8738-496a-9366-1271ebefc1e6"
 }
 
-variable "tenant_id" {
+variable "tenant_id_value" {
   type =string
   default = "1f2519d6-a33d-4336-abb5-8085bcb587d6"
 }
