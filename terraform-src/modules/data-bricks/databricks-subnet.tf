1f0334d6bf7b06c694dff1d0fcd414b2a0ac2e30
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-11-12 21:57:28 +0100

    rename subnet to be unique in network

--- terraform-src/modules/data-bricks/databricks-subnet.tf
+++ terraform-src/modules/data-bricks/databricks-subnet.tf
@@ -1,18 +1,11 @@
 resource "azurerm_subnet" "private_snet" {
-  name                 = "subnet-private"
+  name                 = "databricks-subnet-private"
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.vnet_name
   address_prefixes     = ["10.0.2.0/24"]
 
-  delegation {
-    name = "databricksprivatermdelegation"
-
-    service_delegation {
-      name = "Microsoft.Databricks/workspaces"
-    }
-  }
 }
-
+/*
 resource "azurerm_subnet" "public_snet" {
   name                 = "subnet-public"
   resource_group_name  = var.resource_group_name
@@ -26,4 +19,4 @@ resource "azurerm_subnet" "public_snet" {
       name = "Microsoft.Databricks/workspaces"
     }
   }
-}
+}*/
--- terraform-src/modules/key-vault/keyvault-subnet.tf
+++ terraform-src/modules/key-vault/keyvault-subnet.tf
@@ -1,5 +1,5 @@
 resource "azurerm_subnet" "private_snet" {
-  name                 = "subnet-private"
+  name                 = "keyvault-subnet-private"
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.vnet_name
   address_prefixes     = [var.subnet_ip_range]
