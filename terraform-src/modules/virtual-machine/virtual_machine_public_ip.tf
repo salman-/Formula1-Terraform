ea17e24af9d3f586f0407250579bc384cdaebb8b
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-11-12 20:38:15 +0100

    rename public-ip of vm

--- terraform-src/modules/virtual-machine/virtual_machine_public_ip.tf
+++ terraform-src/modules/virtual-machine/virtual_machine_public_ip.tf
@@ -1,5 +1,5 @@
 resource "azurerm_public_ip" "public_ip_of_firewall_of_storage_account_subnet" {
-  name                = "testpip"
+  name                = "vm-public-ip"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
