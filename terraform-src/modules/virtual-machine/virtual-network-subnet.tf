ba98d82dceedcc2aabc34a47514fb22189c4fd33
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-11-12 20:43:29 +0100

    format and rename the resources

--- terraform-src/modules/virtual-machine/virtual-network-subnet.tf
+++ terraform-src/modules/virtual-machine/virtual-network-subnet.tf
@@ -1,12 +1,12 @@
 resource "azurerm_subnet" "virtual_network_subnet" {
-  name                 = "internal"
+  name                 = "vm_subnet"
   resource_group_name      = var.resource_group_name
   virtual_network_name = var.vnet_name
   address_prefixes     = [var.subnet_ip_range]
 }
 
 resource "azurerm_network_interface" "virtual_network_nic" {
-  name                = "Test-VM-nic"
+  name                = "vm_nic"
   resource_group_name      = var.resource_group_name
   location                 = var.resource_group_location
 
--- terraform-src/modules/virtual-machine/virtual_machine.tf
+++ terraform-src/modules/virtual-machine/virtual_machine.tf
@@ -1,10 +1,10 @@
 resource "azurerm_windows_virtual_machine" "test_virtual_machine" {
-  name                = "test-vm"
-  location            = var.resource_group_location
-  resource_group_name = var.resource_group_name
-  size                = "Standard_F2"
-  admin_username      = "adminuser"
-  admin_password      = "P@$$w0rd1234!"
+  name                  = "vm"
+  location              = var.resource_group_location
+  resource_group_name   = var.resource_group_name
+  size                  = "Standard_F2"
+  admin_username        = "adminuser"
+  admin_password        = "P@$$w0rd1234!"
   network_interface_ids = [
     azurerm_network_interface.virtual_machine_network_interface.id,
   ]
@@ -31,13 +31,14 @@ resource "azurerm_network_interface" "virtual_machine_network_interface" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.virtual_network_subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.vm_public_ip.id
+    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
   }
 }
 
 resource "azurerm_public_ip" "vm_public_ip" {
-  name                = "vm_public_ip"
+  name                = "vm-public-ip-for-rdp"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
+  sku = "Standard"
 }
