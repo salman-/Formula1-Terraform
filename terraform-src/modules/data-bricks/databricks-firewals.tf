da8ec3a369ff92a13c622b43b0320231ae1b36bf
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-11-13 09:25:59 +0100

    diasble network rules for data-bricks

--- terraform-src/modules/data-bricks/databricks-firewals.tf
+++ terraform-src/modules/data-bricks/databricks-firewals.tf
@@ -1,3 +1,4 @@
+/*
 resource "azurerm_network_security_group" "private_empty_nsg" {
   name                = "firewall-private"
   location            = var.resource_group_location
@@ -7,4 +8,4 @@ resource "azurerm_network_security_group" "private_empty_nsg" {
 resource "azurerm_subnet_network_security_group_association" "private_nsg_asso" {
   subnet_id                 = azurerm_subnet.private_snet.id
   network_security_group_id = azurerm_network_security_group.private_empty_nsg.id
-}
+}*/
