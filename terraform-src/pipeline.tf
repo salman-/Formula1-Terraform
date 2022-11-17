32a0a6500d618a43d3be0a6e3b19dd9e2111101d
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-11-17 20:53:44 +0100

    pass username and passowrd of vm

--- terraform-src/pipeline.tf
+++ terraform-src/pipeline.tf
@@ -26,6 +26,8 @@ module "virtual_machine" {
   vnet_name        = module.virtual_network.virtual_network_name
   network_ip_range = module.virtual_network.virtual_network_ip_range
   subnet_ip_range  = "10.0.1.0/24"
+  username         = "adminuser"
+  password         = "adminuseR12345!"
 }
 
 module "storage_account" {
@@ -47,6 +49,10 @@ module "databricks" {
   subnet_ip_prefix        = "10.0.3.0/24"
 }
 
+module "datafactory" {
+  source = "./modules/data-factory"
+}
+/*
 module "keyvault" {
   source = "./modules/key-vault"
 
@@ -61,28 +67,20 @@ module "keyvault" {
   tenant_id_value       = var.tenantId
 }
 
-module "datafactory" {
-  source = "./modules/data-factory"
-}
-/*
 module "database" {
   source                  = "./modules/database"
   resource_group_name     = azurerm_resource_group.main_resource_group.name
   resource_group_location = azurerm_resource_group.main_resource_group.location
   vnet_name               = azurerm_virtual_network.virtual_network.name
 }
-
-module "private_link" {
-  source             = "./modules/private_link"
-  virtual_network_id = module.virtual_network.virtual_network_id
-  subnet_id          = module.storage_account.storage_account_subnet_id
-  name = "storage"
-  zone_name = azurerm_private_dns_zone.dns_zone.name
-  private_link_enabled_resource_id = module.storage_account.storage_account_id
-  subresource_names = ["blob"]
-
-    depends_on = [
-      azurerm_private_dns_zone.dns_zone
-    ]
-}
 */
+module "private_link" {
+  source                           = "./modules/private_link"
+  virtual_network_id               = module.virtual_network.virtual_network_id
+  asset_name_which_use_endpoint    = module.storage_account.storage_account_name
+  subnet_id                        = module.storage_account.storage_account_subnet_id
+  name                             = "storage"
+  zone_name                        = azurerm_private_dns_zone.dns_zone.name
+  private_link_enabled_resource_id = module.storage_account.storage_account_id
+  subresource_names                = ["blob"]
+}
