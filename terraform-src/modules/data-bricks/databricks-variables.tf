dba4c44aac15f394fa483a3a2a1d686b040565f9
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-11-17 21:35:01 +0100

    delete unused variables

--- terraform-src/modules/data-bricks/databricks-variables.tf
+++ terraform-src/modules/data-bricks/databricks-variables.tf
@@ -8,30 +8,6 @@ variable "resource_group_location" {
   default = "West Europe"
 }
 
-variable "storage_account_name" {
-  type    = string
-  default = "forexpricestorageaccount"
-}
-
-variable "datalake_name" {
-  type    = string
-  default = "datalake"
-}
-
-variable "vnet_name" {
-  type    = string
-  default = "my-vnet"
-}
-
-variable "virtual_network_id" {
-  type    = string
-  default = "To be set"
-}
-
-variable "subnet_ip_prefix" {
-  default = "10.0.2.0/24"
-}
-
 variable "databricks_name" {
   default = "formula1-databricks"
 }
