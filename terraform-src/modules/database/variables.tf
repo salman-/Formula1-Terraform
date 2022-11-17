3da7d0d449394aebc0a5de8f461de83222be0c84
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-11-17 22:00:01 +0100

    remove unused variables

--- terraform-src/modules/database/variables.tf
+++ terraform-src/modules/database/variables.tf
@@ -8,16 +8,6 @@ variable "resource_group_location" {
   default = "West Europe"
 }
 
-variable "vnet_name" {
-  type = string
-  default = "forex-price-vnet"
-}
-
-variable "virtual_network_id" {
-  type =string
-  default = "To be set"
-}
-
 locals {
   prefix_database = "database"
   prefix_database_server = "mssql-server"
