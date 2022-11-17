0140cad33db8e35ffcb621010ec66c53de433bd1
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-11-17 21:59:09 +0100

    remove files

--- terraform-src/modules/key-vault/keyvault-subnet.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_subnet" "private_snet" {
-  name                 = "keyvault-subnet-private"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = var.vnet_name
-  address_prefixes     = [var.subnet_ip_range]
-}
