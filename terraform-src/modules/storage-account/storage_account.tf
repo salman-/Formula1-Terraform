d7b44cd996000ff276a33500f930d6366bfda353
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-11-17 20:52:21 +0100

    format

--- terraform-src/modules/storage-account/storage_account.tf
+++ terraform-src/modules/storage-account/storage_account.tf
@@ -1,11 +1,11 @@
 resource "azurerm_storage_account" "storage_account" {
-  name                     = var.storage_account_name
-  resource_group_name      = var.resource_group_name
-  location                 = var.resource_group_location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind             = "StorageV2"
-  is_hns_enabled           = "true"
+  name                          = var.storage_account_name
+  resource_group_name           = var.resource_group_name
+  location                      = var.resource_group_location
+  account_tier                  = "Standard"
+  account_replication_type      = "LRS"
+  account_kind                  = "StorageV2"
+  is_hns_enabled                = "true"
 }
 
 resource "azurerm_storage_data_lake_gen2_filesystem" "data_lake_storage" {
