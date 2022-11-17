117c734179c2d865053823ee7d045840a4e1eeba
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-11-17 20:49:35 +0100

    only port 22 should be open on vm

--- terraform-src/modules/virtual-machine/virtual_macine_subnet_traffic_rule.tf
+++ terraform-src/modules/virtual-machine/virtual_macine_subnet_traffic_rule.tf
@@ -10,7 +10,7 @@ resource "azurerm_network_security_group" "traffic_rules" {
     access                     = "Allow"
     protocol                   = "*"
     source_port_range          = "*"
-    destination_port_range     = "3389"
+    destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
