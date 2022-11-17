f60eb316170b4e00fbda44dcc292297551f01ec6
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-11-17 20:52:05 +0100

    storage-account name is used to setup private-link

--- terraform-src/modules/storage-account/storage_account_output.tf
+++ terraform-src/modules/storage-account/storage_account_output.tf
@@ -5,3 +5,7 @@ output "storage_account_subnet_id" {
 output "storage_account_id" {
   value = azurerm_storage_account.storage_account.id
 }
+
+output "storage_account_name" {
+  value = azurerm_storage_account.storage_account.name
+}
