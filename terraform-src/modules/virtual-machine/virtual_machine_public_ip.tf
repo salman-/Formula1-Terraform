5a5329e388982de20a0a0bb50f4b728014c8b489
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-12-08 19:37:05 +0100

    set ip dynamically

--- terraform-src/modules/virtual-machine/virtual_machine_public_ip.tf
+++ terraform-src/modules/virtual-machine/virtual_machine_public_ip.tf
@@ -2,6 +2,6 @@ resource "azurerm_public_ip" "public_ip_of_firewall_of_storage_account_subnet" {
   name                = "vm-public-ip"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
-  allocation_method   = "Static"
+  allocation_method   = "Dynamic"
   sku                 = "Standard"
 }
