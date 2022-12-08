59bc30b1cbf9d33836a7daf1a091ab0994971cce
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-12-08 15:53:22 +0100

    create keyvault as part of pipeline

--- terraform-src/pipeline.tf
+++ terraform-src/pipeline.tf
@@ -51,12 +51,12 @@ module "databricks" {
 module "datafactory" {
   source = "./modules/data-factory"
 }
-/*
+
 module "keyvault" {
   source = "./modules/key-vault"
 
   project_name = "formulaeins"
-}*/
+}
 
 module "database" {
   source = "./modules/database"
