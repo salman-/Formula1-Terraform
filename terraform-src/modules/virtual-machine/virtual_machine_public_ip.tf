0913697dcb1074747fe4807161b9d6a3b8e3b1c6
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-12-08 23:52:32 +0100

    remove unused files

--- terraform-src/modules/virtual-machine/virtual_machine_public_ip.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_public_ip" "public_ip_of_firewall_of_storage_account_subnet" {
-  name                = "vm-public-ip"
-  location            = var.resource_group_location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Dynamic"
-  sku                 = "Basic"
-}
