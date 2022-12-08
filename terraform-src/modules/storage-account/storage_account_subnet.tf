8e964474d0ca4b8ad755e1bdc57b13a8b26a4690
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-12-08 08:08:24 +0100

    storage does not need a subnet

--- terraform-src/modules/storage-account/storage_account_subnet.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_subnet" "storage_account_subnet" {
-  name                 = "storage_account_subnet"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = var.vnet_name
-  address_prefixes     = [var.subnet_ip_range]
-}
