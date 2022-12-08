2bdce93987ff4fb2e1013b828f7c761116171e70
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-12-08 07:50:06 +0100

    format the code

--- terraform-src/modules/virtual-machine/virtual-network-subnet.tf
+++ terraform-src/modules/virtual-machine/virtual-network-subnet.tf
@@ -1,14 +1,14 @@
 resource "azurerm_subnet" "virtual_network_subnet" {
   name                 = "vm_subnet"
-  resource_group_name      = var.resource_group_name
+  resource_group_name  = var.resource_group_name
   virtual_network_name = var.vnet_name
   address_prefixes     = [var.subnet_ip_range]
 }
 
 resource "azurerm_network_interface" "virtual_network_nic" {
   name                = "vm_nic"
-  resource_group_name      = var.resource_group_name
-  location                 = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_location
 
   ip_configuration {
     name                          = "testconfiguration1"
