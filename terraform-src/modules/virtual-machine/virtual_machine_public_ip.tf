83ab8cdab7d3ba6d7d160bb67b9f41d2329bb98a
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-12-08 19:54:14 +0100

    disable vm's nic

--- terraform-src/modules/virtual-machine/virtual_machine_public_ip.tf
+++ terraform-src/modules/virtual-machine/virtual_machine_public_ip.tf
@@ -3,5 +3,5 @@ resource "azurerm_public_ip" "public_ip_of_firewall_of_storage_account_subnet" {
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
   allocation_method   = "Dynamic"
-  sku                 = "Standard"
+  sku                 = "Basic"
 }
