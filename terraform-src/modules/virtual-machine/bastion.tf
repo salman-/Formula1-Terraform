3f07e14992b5ccf3efa683ae88a5ee7bed98e63f
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-12-08 23:51:16 +0100

    add bastion to connect to network

---
+++ terraform-src/modules/virtual-machine/bastion.tf
@@ -0,0 +1,27 @@
+resource "azurerm_subnet" "bastion_subnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.vnet_name
+  address_prefixes     = [var.bastion_subnet_range]
+}
+
+
+resource "azurerm_public_ip" "bastion_public_ip" {
+  name                = "vm-public-ip"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "bastion" {
+  name                = "bastion"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.bastion_subnet.id
+    public_ip_address_id = azurerm_public_ip.bastion_public_ip.id
+  }
+}
