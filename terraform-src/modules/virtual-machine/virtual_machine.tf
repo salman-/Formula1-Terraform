71741b60958fae741c90b0274af92f17c47d9198
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-12-08 23:53:32 +0100

    remove unused public ip

--- terraform-src/modules/virtual-machine/virtual_machine.tf
+++ terraform-src/modules/virtual-machine/virtual_machine.tf
@@ -6,9 +6,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   admin_username                  = var.virtual_machine_username
   admin_password                  = var.virtual_machine_password
   disable_password_authentication = false
-  network_interface_ids           = [
-    azurerm_network_interface.virtual_machine_network_interface.id,
-  ]
+  network_interface_ids           = [azurerm_network_interface.virtual_machine_network_interface.id,]
 
 
   os_disk {
@@ -33,14 +31,12 @@ resource "azurerm_network_interface" "virtual_machine_network_interface" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.virtual_network_subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
   }
 }
 
-resource "azurerm_public_ip" "vm_public_ip" {
-  name                = "vm-public-ip"
-  location            = var.resource_group_location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Dynamic"
-  sku                 = "Basic"
+resource "azurerm_subnet" "virtual_network_subnet" {
+  name                 = "vm_subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.vnet_name
+  address_prefixes     = [var.subnet_ip_range]
 }
