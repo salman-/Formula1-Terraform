009af947affeddd5713c03de1b07f3ea4017bdc7
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-12-08 07:49:59 +0100

    format the code

--- terraform-src/modules/virtual_network/virtual-network.tf
+++ terraform-src/modules/virtual_network/virtual-network.tf
@@ -1,6 +1,13 @@
 resource "azurerm_virtual_network" "virtual_network" {
   name                = var.vnet_name
-  address_space       = [var.virtual_network_address_space]
+  address_space       = ["10.0.0.0/16"]
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_subnet" "custom_subnets" {
+  name                 = "custom_subnets"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.virtual_network.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
