cd08358cb4c9ef0b6df923b0d7ad829c43fd93c8
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-12-08 07:42:01 +0100

    remove unused variable

--- terraform-src/modules/storage-account/storage_account_variables.tf
+++ terraform-src/modules/storage-account/storage_account_variables.tf
@@ -13,15 +13,6 @@ variable "vnet_name" {
   default = "my-vnet"
 }
 
-variable "subnet_ip_range" {
-  type =string
-  default = "10.0.2.0/24"
-}
-
 variable "storage_account_name" {
-  default = ""
-}
-
-variable "network_ip_range" {
   default = ""
 }
