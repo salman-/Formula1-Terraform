8f23085e86777b9f9ad0ca4c8e8629fe284cb80e
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-12-09 08:23:53 +0100

    adf is avaialble in public network

--- terraform_src/modules/data_factory/data_factory.tf
+++ terraform_src/modules/data_factory/data_factory.tf
@@ -2,6 +2,7 @@ resource "azurerm_data_factory" "data_factory" {
   name                = "formulaeinsdatafactory"
   resource_group_name = var.resource_group_name
   location            = var.resource_group_location
+  #public_network_enabled = false
 
   lifecycle {
     prevent_destroy = true
