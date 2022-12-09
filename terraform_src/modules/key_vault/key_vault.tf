3918560dd5343074aa6dfb12817ffeed472492bf
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-12-09 19:20:25 +0100

    update key vault

--- terraform_src/modules/key_vault/key_vault.tf
+++ terraform_src/modules/key_vault/key_vault.tf
@@ -17,7 +17,7 @@ resource "azurerm_key_vault_access_policy" "access_policy" {
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
 
-  secret_permissions = ['Backup', 'Delete', 'Get', 'List', 'Purge', 'Recover', 'Restore', 'Set',]
+  secret_permissions = ["Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set",]
 }
 
 resource "azurerm_key_vault_secret" "client_id" {
