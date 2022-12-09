ed6850c222f0cda53293ba0d02e1967a4f95f3b8
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-12-09 08:23:21 +0100

    add dns zone

--- terraform_src/dns_zone.tf
+++ terraform_src/dns_zone.tf
@@ -1,6 +1,3 @@
-# As long as there is no private endpoint, we do not need this service
-
-/*
 resource "azurerm_private_dns_zone" "dns_zone" {
   name                = "privatelink.blob.core.windows.net"
   resource_group_name = var.resource_group_name
@@ -10,7 +7,6 @@ resource "azurerm_private_dns_zone_virtual_network_link" "network_link" {
   name                  = "vnet_link"
   resource_group_name   = var.resource_group_name
   private_dns_zone_name = azurerm_private_dns_zone.dns_zone.name
-  virtual_network_id    = azurerm_virtual_network.virtual_network.id
+  virtual_network_id    = module.virtual_network.virtual_network_id
 
 }
-*/
