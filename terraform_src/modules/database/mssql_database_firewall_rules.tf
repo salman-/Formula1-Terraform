85d6291e179f37ba4d31391583b6f34760b563b3
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-12-14 19:20:55 +0100

    Update database rule

--- terraform_src/modules/database/mssql_database_firewall_rules.tf
+++ terraform_src/modules/database/mssql_database_firewall_rules.tf
@@ -5,6 +5,13 @@ resource "azurerm_mssql_firewall_rule" "my_ip_rule" {
   end_ip_address      = "78.54.131.237"
 }
 
+resource "azurerm_mssql_firewall_rule" "my_ip_rule" {
+  name                = "my_ip_rule"
+  server_id           = azurerm_mssql_server.sql_server.id
+  start_ip_address    = "40.74.27.172"
+  end_ip_address      = "40.74.27.172"
+}
+
 resource "azurerm_mssql_firewall_rule" "virtual_network_rule" {
   name                = "virtual_network_rule"
   server_id           = azurerm_mssql_server.sql_server.id
