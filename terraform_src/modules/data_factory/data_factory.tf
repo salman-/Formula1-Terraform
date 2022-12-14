e764efbcbf21b3e07954de9f4dd29732a3a9124c
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-12-14 11:19:06 +0100

    Update Datafactory

--- terraform_src/modules/data_factory/data_factory.tf
+++ terraform_src/modules/data_factory/data_factory.tf
@@ -4,6 +4,18 @@ resource "azurerm_data_factory" "data_factory" {
   location            = var.resource_group_location
   #public_network_enabled = false
 
+  identity {
+    type = "SystemAssigned"
+  }
+
+  github_configuration {
+    account_name    = "salman-"
+    branch_name     = "main"
+    git_url         = "https://github.com/salman-/Formula1-DataFactory"
+    repository_name = "main"
+    root_folder     = "/"
+  }
+
   lifecycle {
     prevent_destroy = true
   }
